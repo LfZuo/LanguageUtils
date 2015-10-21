bc550a4e4fee73a4d1158c0ac6a728ad3e95c7f8
LfZuo <lfzuo.sunny@gmail.com>
2015-10-21 13:51:30 +0800

    default project

---
+++ LanguageUtils/LanguageUtils/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LanguageUtils
+//
+//  Created by 左立峰 on 15/10/21.
+//  Copyright © 2015年 左立峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
